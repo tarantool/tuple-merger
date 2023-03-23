b69b9b5401c93ad6452cfcb289cc361c453be759
Ilya Grishnov <ilya.grishnov@tarantool.org>
2023-03-23 14:48:16 +0300

    api: add hardcoded versioning support
    Added the _VERSION variable to the exported table.
    Is part of the task [1].
    
    1. github.com/tarantool/roadmap-internal/issues/204

---
+++ src/merger/version.h
@@ -0,0 +1,10 @@
+#ifndef TUPLE_MERGER_VERSION_H_INCLUDED
+#define TUPLE_MERGER_VERSION_H_INCLUDED
+/*
+* Сontains the module version.
+* Requires manual update in case of release commit.
+*/
+
+#define TUPLE_MERGER_VERSION "0.0.4"
+
+#endif /* TUPLE_MERGER_VERSION_H_INCLUDED */
